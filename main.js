180cbc6cf02db48ac8e6e2f24de72368fbbee54f
GulyaevSergey <109235648+GulyaevSergey@users.noreply.github.com>
2022-08-14 23:25:56 +0700

    adding a task commit

--- main.js
+++ main.js
@@ -1,16 +1,54 @@
-var form = document.getElementById("addForm");
+const form = document.getElementById("addForm");
+const itemsList = document.getElementById("items");
+console.log("itemsList", itemsList);
 
 
 form.addEventListener("submit", addItem);
 
 function addItem(e){
+
+    // Отсеняем отправку формы
     e.preventDefault();
 
-    var newItemInput = document.getElementById("newItemText");
+    // Находим импут с текстом для добавления новой задачи
+    const newItemInput = document.getElementById("newItemText");
+
+    // Получаем текст из инпута
+    const newItemText = newItemInput.value;
+
+    // Создаем элемент для новой задачи
+    const newElement = document.createElement("li");
+    newElement.className = "list-group-item";
+
+    // Добавляем текст в новый элемент
+    const newElementText = document.createTextNode(newItemText);
+    newElement.appendChild(newElementText);
     
-    var newItemText = newItemInput.value;
-    console.log("newItemText", newItemText);
+
+    // Создаем кнопку
+    const deleteBtn = document.createElement("button");
+
+    // Добавляем текст в кнопку
+    deleteBtn.appendChild(document.createTextNode("Удалить"));
+
+    // Добавляем класс для кнопки
+    deleteBtn.className = "btn btn-light btn-sm float-right";
+
+    // Добавляем дата атрибут
+    deleteBtn.dataset.action = "delete";
+
+    // Помещаем кнопку внутрь тега li
+    newElement.appendChild(deleteBtn);
+    console.log("newElement", newElement);
+
+    // Добавляем новую задачу в список
+    itemsList.prepend(newElement);
+
+    // Очищаем импут после добавления новой задачи
+    newItemInput.value = "";
+
+
     
-    
+
 }
 
